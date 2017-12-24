ea60d100602e1fedfcfc12663c1bf161f38fc8d1
Aleksander Chromik <achromik@gmail.com>
2017-12-24 11:33:40 +0100

    fix js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,11 @@ console.log('ready');
 
 
 $('#list').find('li').mouseover(function(){
-    $(this).parent('ol').find('li').find('div').removeClass('active')
-    $(this).parent('ol').find('li').removeClass('active-item')
-    $(this).addClass('active-item')
-
-    $(this).find('div').addClass('active')
-
+    $(this).parent('ol').find('li').find('div').removeClass('active');
+    $(this).parent('ol').find('li').removeClass('active-item');
+    $(this).addClass('active-item');
+    $(this).find('div').addClass('active');
     console.log('click');
-})
+});
 
 });
